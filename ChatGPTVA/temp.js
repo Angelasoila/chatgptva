a755000debf79b11aee761fafeee4a869dd2eea9
Angelasoila <103871988+Angelasoila@users.noreply.github.com>
2024-03-08 17:11:17 +0300

    Add files via upload

---
+++ ChatGPTVA/temp.js
@@ -0,0 +1,9 @@
+//declare the temperature in kelvin
+let kelvin= 293
+celcius= kelvin-273
+//temperature converted to celcius
+farenheit = celcius*(9/5)+ 32
+//temperature converted to farenheit
+Math.floor(farenheit)
+//round off the farenheit value
+console.log(`The temperature is TEMPERATURE ${farenheit} degrees farenheit`)
