8b095d1fd00baf661a756ce0a08cc89fea31d81d
Angelasoila <103871988+Angelasoila@users.noreply.github.com>
2024-03-08 17:13:15 +0300

    Delete ChatGPTVA directory

--- ChatGPTVA/temp.js
+++
@@ -1,9 +0,0 @@
-//declare the temperature in kelvin
-let kelvin= 293
-celcius= kelvin-273
-//temperature converted to celcius
-farenheit = celcius*(9/5)+ 32
-//temperature converted to farenheit
-Math.floor(farenheit)
-//round off the farenheit value
-console.log(`The temperature is TEMPERATURE ${farenheit} degrees farenheit`)
